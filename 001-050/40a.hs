e12854ac4f1ccd3cd646d1debe4db920d003c9f2
Bert Douglas <georgehdouglas@gmail.com>
2014-01-04 10:28:12 -0800

    solution 40a paremeterize maximum exponent make dsplit handle zero and negative numbers add comments on how to improve further

--- 001-050/40a.hs
+++ 001-050/40a.hs
@@ -15,11 +15,15 @@ d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 import Data.List
 import qualified Data.Vector.Unboxed as DVU
 
+max_exp = 6
+
 dsplit :: Int -> [Int]
 dsplit n = dsplit' n []
   where
     dsplit' n s
       | n > 0     = dsplit' rest (first:s)
+      | n < 0     = []
+      | s == []   = [0]
       | otherwise = s
         where
           first = mod n 10
@@ -28,18 +32,36 @@ dsplit n = dsplit' n []
 digits :: DVU.Vector Int
 digits =
   DVU.fromList
-  $ ([0] ++)
-  $ take (10^6)
+  $ take ((10^max_exp) +1)
   $ concat
   $ map dsplit 
-  $ [1..]
+  $ [0..]
 
 solve =
   product
   $ map (digits DVU.!)
-  $ map (10^) [0..6]
+  $ map (10^) [0..max_exp]
 
 main = do
   print $ DVU.slice 0 100 digits
   print solve
+
+{-
+This is a brute force method.
+It stores the entire array of n=10^6 digits.
+Then looks them up.
+obviously this won't work for larger n.  The array is consuming
+about 8 megabytes, which is trivial.  But I only have 3 gigabytes
+total.  A value of n=9 would surely fail due to lack of memory.
+It would also be 1000 x slower.
+
+Next step would be to scan the digits and pick off the desired
+ones as they are produced, so that it is not necessary to store 
+a large array. This would reduce memory use to a small constant
+amount.  But it would still be O(10^n) with time.
+
+It would also be possible, with a little mental effort, to make
+a closed form, constant time solution.
+-}
+
 
